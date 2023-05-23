4aefc3dccdcea1a6b2341401e6e2dee7dd8dae75
Mehmet Ali Sivri <mehmetalisivri.dev@gmail.com>
2023-05-23 23:16:23 +0300

    uzmanlara soruna php eklendi

--- HastaneSitesi/php/hasta_soru_ilet.php
+++ HastaneSitesi/php/hasta_soru_ilet.php
@@ -5,17 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
-    <title>Doktor Mesaj</title>
+    <title>Hastalar</title>
 </head>
 <body style="margin-top: 50px;">
 <div class="container">
     <div class="row">
         <?php 
-        session_start();
-        if(isset($_POST["cevap_submit"])){
+        if(isset($_POST["gonder"])){
             // --------------------------------------------------------------------
             // Kullabıcı girişine yazılan kod ile hiç bir bilgi içeriye alınmayacak 
-            
             function test_input($data){ 
                 $data = trim($data);
                 $data = stripcslashes($data);
@@ -38,38 +36,43 @@
                 die("Bağlantı hatası: ".$conn->connect_error);
             }
                
-                //buraya id'ler cekilcek ...
 
-
-            $hastaemail= test_input($_POST["hasta_eposta"]);
-            $doktorcevap = test_input($_POST["cevap"]);
-            $doktorEmail = $_SESSION["doktor_eposta"];
-            $doktorId = $_SESSION["doktor_id"];
-         
+            $hasta_eposta = test_input($_POST["email"]);
+            $poliklinik = test_input($_POST["poliForm"]);
+            $soru = test_input($_POST["message"]);
             // ------------------------------------------------------------------------------------
-            $sql = "INSERT INTO doktor_cevap (`doktor_id`,`cevap`,`hasta_eposta`) VALUES
+            $sql = "INSERT INTO hasta_soru (`hasta_eposta`, `poliklinik`, `soru`) VALUES
                 (?,?,?)";
             
+           
             // ifademiniz hazırlıyoruz. parse edilir ve DB sunucu saklanır. Tekrar tekrar kullanılabilir
             
             $stmt = mysqli_prepare($conn,$sql);
             // sorgumuzda çalıştırılacak parametreleri gönderiyoruz  is -> i = int ve s = string
-            mysqli_stmt_bind_param($stmt, "iss", $doktorId ,$doktorcevap,$hastaemail);
+            mysqli_stmt_bind_param($stmt, "sss", $hasta_eposta,$poliklinik,$soru);
             // sorgu çalıştır
             mysqli_stmt_execute($stmt);
 
+           
             $result = mysqli_stmt_get_result($stmt);
 
-
+            // ----------b--------------------------------------------------------burya doktor siteden mesaj ile geri donsun yada arasın hastasını
             if($result == false){
-                         
-                    echo("<div class='alert alert-success' role='alert'>
-                    İletiniz Başarıyla Gönderilmiştir.
-                  </div>");
-                   
+             
+                    //buraya alert olarak aranacagınız yaz yada doktor mesajlaşması için bişiler  yap
+                echo("<div class='alert alert-success' role='alert'>
+                iletiniz Başarıyla Gönderilmiştir . ilgili konu ile uzman doktorumuz tarafından size en kısa zamanda geri dönüş  sağlanacakır.
+              </div>");
+                
+              header("Refresh: 2; ../index.html");
+
             }
             else{
-                echo "Hatalı Işlem Yaptınız Lütfen Tekrar Deneyiniz.";
+                echo("<div class='alert alert-danger' role='alert'>
+                Hatalı Işlem Yaptınız Lütfen Tekrar Deneyiniz.
+              </div>");
+
+              header("Refresh: 2; ../index.html");
             }
 
         }
